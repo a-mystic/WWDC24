c42615f7bdcad1d4ce322a22e9c5a2b132b57b83
a-mystic <totocaca@naver.com>
2024-01-02 19:19:42 +0900

    add some posture algorithm.

--- Your Speech.swiftpm/PostureRecognitionView.swift
+++ Your Speech.swiftpm/PostureRecognitionView.swift
@@ -63,27 +63,29 @@ extension PostureRecognitionViewController: ARSessionDelegate {
                let leftShoulderPos = bodyAnchor.skeleton.modelTransform(for: .leftShoulder)?.columns.3,
                let rightFootPos = bodyAnchor.skeleton.modelTransform(for: .rightFoot)?.columns.3,
                let leftFootPos = bodyAnchor.skeleton.modelTransform(for: .leftFoot)?.columns.3 {
-                let rightHandPosition = rightHandPos.y
-                let leftHandPosition = leftHandPos.y
                 let shoulderDistance = abs(leftShoulderPos.x - rightShoulderPos.x)
                 let footDistance = leftFootPos.x - rightFootPos.x
                 let isCrossLeg = footDistance < 0
                 
                 postureManager.updatePosture("foot distance: \(footDistance)")
                 if postureManager.currentPostureMode == .initial {
-                    if rightHandPosition < rightShoulderPos.y * 0.85 &&
-                        leftHandPosition < leftShoulderPos.y * 0.85 &&
+                    if rightHandPos.y < rightShoulderPos.y * 0.85 &&
+                        leftHandPos.y < leftShoulderPos.y * 0.85 &&
                         footDistance > shoulderDistance * 1.55 && footDistance < shoulderDistance * 2.2 {
-                        postureManager.updatePosture("Initial Okay chagne mode after 3 second.")
+                        postureManager.updatePosture("Initial Okay chagne mode after 5 second.")
                         postureManager.isChanging = true
                         DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
                             self.postureManager.changeModeToRehearsal()
                         }
-                    } else {
-                        postureManager.updatePosture("spread your legs shoulder width apart")
+                    } else if footDistance < shoulderDistance * 1.55 || footDistance > shoulderDistance * 2.2 {
+                        postureManager.updatePosture("Keep the space between your legs about shoulder width.")
+                    } else if rightHandPos.y > rightShoulderPos.y * 0.85 {
+                        postureManager.updatePosture("Down your right Hand")
+                    } else if leftHandPos.y > leftShoulderPos.y * 0.85 {
+                        postureManager.updatePosture("Down your left hand")
                     }
                 } else if postureManager.currentPostureMode == .rehearsal {
-                    if rightHandPosition > rightShoulderPos.y * 0.85 || leftHandPosition > leftShoulderPos.y * 0.85 {
+                    if rightHandPos.y > rightShoulderPos.y * 0.85 || leftHandPos.y > leftShoulderPos.y * 0.85 {
                         postureManager.updatePosture("Over shoulder")
                     } else {
                         postureManager.updatePosture("Not")
