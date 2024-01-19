7f7e1d7f47c052c74fd49365708cc7559b7b589f
a-mystic <totocaca@naver.com>
2024-01-19 20:24:37 +0900

    edit texts.

--- Your Speech.swiftpm/Utilities/TextConstants.swift
+++ Your Speech.swiftpm/Utilities/TextConstants.swift
@@ -9,30 +9,31 @@ import Foundation
 
 struct TextConstants {
     static let introTexts = [
-        "🎙️ Your Speech",
-        "💡\nI know you have great idea.",
-        "And I ensure your idea can change world.",
-        "And third.",
-        "And forth."
+        "🎙️\nYour Speech",
+        "💡\nI know you have great ideas.",
+        "🌎\nAnd I'm sure your ideas will change the world.",
+        "💭\nBut ideas that exist only in imagination are meaningless.",
+        "✉️\nIdeas are meaningful only when they are well communicated to people.",
+        "If you want to convey your idea to people, click the button below."
     ]
     
     static let voiceAndFaceText = """
-    When giving a presentation, your voice is important.
-    You need to practice recognizing whether your voice is trembling or your pronunciation is incorrect.
-    Also, non-verbal aspects are important when giving a presentation.
-    While speaking your voice, your facial expressions and eye movements will be analyzed at the same time (it's strange if you frown too much or move your eyes too much when speaking).
-    Additionally, it is important to write a script and practice your script before you begin your presentation.
-    Write a script for your presentation, shorten the parts you think are important, and test whether you present it properly.
-    Lastly, during the analysis, a chart appears at the bottom, and there is a secret hidden in it...
+    When giving a presentation, not only your voice and voice but also non-verbal communication is important.
+    In this chapter, we analyze your voice and facial features. (Click the button in the top right for details)
+    Lastly, it is important to write and practice a script before you begin your presentation.
+    Write a script for your presentation, shorten the parts you think are important, and present it as if it were real.
+    When you're ready, tap the button below.
     """
     
     static let postureText = """
-    This chapter can be played by two or more people, including yourself and friends or family.
-    This chapter will provide you with the ability to correct your presentation posture in the non-verbal aspect.
+    I mentioned earlier that non-verbal communication is important when giving a presentation.
+    This chapter analyzes your presentation posture in terms of non-verbal aspects. (Click the button in the top right for details)
     The analysis consists of two steps.
-    1. ready: Check your posture at the start of your presentation. If the starting position for the presentation is appropriate, after a 5-second countdown, move on to the next stage, the rehearsal stage.
-    2. Rehearsal: You rehearse as if you were giving an actual presentation. (Considering users who play alone, the game automatically moves to the feedback screen after 35 seconds. If you have completed your presentation before then, just press the Finish button.)
+    1. ready: Check your posture at the start of your presentation. If your posture is appropriate, count down 5 seconds and then move on to the next stage, the rehearsal stage.
+    2. Rehearsal: You rehearse as if you were giving an actual presentation.
+    Before you start your presentation, think about how you would like to pose during your presentation.
+    When you're ready, tap the button below.
     """
     
-    static let finishText = "I hope your ideas reach the world"
+    static let finishText = "I hope your ideas reach the world."
 }
